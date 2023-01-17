95ba175600be81f881f8e5b5580e52936f043ccc
DaurB <biboldauren@gmail.com>
2023-01-18 00:00:04 +0600

    test

--- src/test/java/tests/WikipediaTests.java
+++ src/test/java/tests/WikipediaTests.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class SearchTests extends TestBase {
+public class WikipediaTests extends TestBase {
 
     @Owner("Bibol Dauren")
     @Test
@@ -56,9 +56,37 @@ public class SearchTests extends TestBase {
         step("Add new language", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/search_lang_button")).click();
             $$(AppiumBy.id("org.wikipedia.alpha:id/wiki_language_title")).findBy(text("ADD LANGUAGE")).click();
-            $$(AppiumBy.id("org.wikipedia.alpha:id/localized_language_name")).findBy(text("Deutsch")).click();
+            $$(AppiumBy.id("org.wikipedia.alpha:id/localized_language_name")).findBy(text("Español")).click();
         });
         step("Verify added language", () ->
-                $$(AppiumBy.className("android.widget.TextView")).findBy(text("Deutsch")).shouldBe(visible));
+                $$(AppiumBy.className("android.widget.TextView")).findBy(text("Español")).shouldBe(visible));
+    }
+
+    @Owner("Bibol Dauren")
+    @Test
+    @DisplayName("Onboarding screen")
+    void getOnBoardingTest() {
+        step("Check 'The Free Encyclopedia …in over 300 languages' text is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
+        });
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+        step("Check 'New ways to explore' text is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("New ways to explore"));
+        });
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+        step("Check 'Reading lists with sync' text is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Reading lists with sync"));
+        });
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+        step("Check 'Send anonymous data' text is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Send anonymous data"));
+        });
     }
 }
